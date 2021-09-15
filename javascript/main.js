58d8d31172c0d0c9495e866f0e36ffd15d743550
jayfullstack <jayfullstack@yahoo.com>
2021-09-15 14:09:48 -0400

    Infinite Scroll function adds more images

--- javascript/main.js
+++ javascript/main.js
@@ -51,5 +51,12 @@ async function getPhotos(){
 	}
 }
 
+// Check to see if scrolling near bottom of page
+window.addEventListener('scroll', () => {
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+		getPhotos();
+	}
+})
+
 // On Load
 getPhotos();
