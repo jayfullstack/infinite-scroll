d2b215231783793a75eb728d235dcd62d78a92cc
jayfullstack <jayfullstack@yahoo.com>
2021-09-15 14:23:22 -0400

    project complete

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'iIHUmKuEILb2wdA0nc-bJmmwkyFE2V5kYHn2Gh7vSIE';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded(){
+	imagesLoaded++;
+	if (imagesLoaded === totalImages){
+		ready = true;
+		loader.hidden = true;
+		console.log('ready =', ready);
+	}
+}
 
 // Helper Function to Set Attributes on DOM Elements
 
@@ -20,19 +33,24 @@ function setAttributes(element, attributes) {
 function displayPhotos(){
 	// Run funciton for each object in photosArray
 	photosArray.forEach((photo) => {
+		imagesLoaded = 0;
+		totalImages = photosArray.length;
+		console.log('total images', totalImages);
 		// Create <a> to link to Unsplash
 		const item = document.createElement('a');
 		setAttributes(item, {
 			href: photo.links.html,
 			target: '_blank',
-		})
+		});
 		// Create <img> for photo
 		const img = document.createElement('img');
 		setAttributes(img, {
 			src: photo.urls.regular,
 			alt: photo.alt_description,
 			title: photo.alt_description
-		})
+		});
+		// Event listener, check when each is finished loading
+		img.addEventListener('load', imageLoaded);
 		// Put <img> inside <a>, then put both
 		// inside img container element
 		item.appendChild(img);
@@ -53,7 +71,8 @@ async function getPhotos(){
 
 // Check to see if scrolling near bottom of page
 window.addEventListener('scroll', () => {
-	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+		ready = false;
 		getPhotos();
 	}
 })
