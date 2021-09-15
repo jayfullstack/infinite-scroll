198eaabcf24080f94f2a1bd8929cf0f2b1d06f46
jayfullstack <jayfullstack@yahoo.com>
2021-09-15 13:00:42 -0400

    hooked up api

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+// Unsplash API
+const count = 10;
+const apiKey = 'iIHUmKuEILb2wdA0nc-bJmmwkyFE2V5kYHn2Gh7vSIE';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Get photos from Unsplash API
+
+async function getPhotos(){
+	try{
+		const response = await fetch(apiUrl);
+		const data =  await response.json();
+		console.log(data);
+	} catch(error){
+		// Catch Error Here
+	}
+}
+
+// On Load
+getPhotos();
